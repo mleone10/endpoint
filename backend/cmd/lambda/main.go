fae5716b3fa084515b03b4fcffd194e50d2b09b2
Mario Leone <mleone10@gmail.com>
2020-09-02 23:23:03 -0400

    Implement API deployment to lambda

---
+++ backend/cmd/lambda/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/handlerfunc"
+	"github.com/mleone10/endpoint/internal"
+)
+
+var adapter *handlerfunc.HandlerFuncAdapter
+
+func init() {
+	adapter = handlerfunc.New(internal.NewServer().ServeHTTP)
+}
+
+func serverHandler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return adapter.Proxy(req)
+}
+
+func main() {
+	lambda.Start(serverHandler)
+}
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module github.com/mleone10/endpoint
 
 go 1.13
 
-require github.com/go-chi/chi v4.1.2+incompatible
+require (
+	github.com/aws/aws-lambda-go v1.19.1
+	github.com/awslabs/aws-lambda-go-api-proxy v0.8.0
+	github.com/go-chi/chi v4.1.2+incompatible
+)
