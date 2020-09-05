9b15242942a6409331e2b7090a6f610d5b6a6cc8
Mario Leone <mleone10@gmail.com>
2020-09-05 19:52:59 -0400

    Move auth middleware code out of auth code

--- backend/internal/user/auth.go
+++ backend/internal/user/auth.go
@@ -3,46 +3,41 @@ package user
 import (
 	"context"
 	"fmt"
-	"net/http"
 
 	firebase "firebase.google.com/go"
 )
 
-// AuthTokenVerifier is a middleware which verifies an Authorization header JWT using the Firebase Admin SDK.
-func AuthTokenVerifier() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		app, err := firebase.NewApp(context.Background(), nil)
-		if err != nil {
-			panic(fmt.Sprintf("error initializing auth middleware: %v", err))
-		}
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Create a request-scoped authentication client.
-			client, err := app.Auth(r.Context())
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			// Verify the token, returning an HTTP 403 if it cannot be.
-			verifiedToken, err := client.VerifyIDToken(r.Context(), r.Header.Get("Authorization"))
-			if err != nil {
-				http.Error(w, "failed to verify authentication token", http.StatusForbidden)
-				return
-			}
-
-			// Create a new context off of the original request context, but add the user ID from the verified token.
-			ctx := NewContextWithID(r.Context(), NewID(verifiedToken.UID))
-			next.ServeHTTP(w, r.Clone(ctx))
-		})
-	}
+// Authenticator is a client object which authenticates credentials against a backend service.
+type Authenticator struct {
+	fbApp *firebase.App
 }
 
-// AuthStubber is a middleware which ignores the provided Authorization header and instead injects a static user ID into the request.
-func AuthStubber() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := NewContextWithID(r.Context(), NewID("testUserID"))
-			next.ServeHTTP(w, r.Clone(ctx))
-		})
+// NewAuthenticator returns a fully initialized Authenticator, or an error if it cannot be created.
+func NewAuthenticator() (*Authenticator, error) {
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing authentication client: %w", err)
 	}
+
+	return &Authenticator{
+		fbApp: app,
+	}, nil
+}
+
+// VerifyJWT verifies a give JWT against a backend service and returns a user ID string if successful.  If the operation fails, or if the token cannot be verified, an error is returned.
+func (a *Authenticator) VerifyJWT(ctx context.Context, jwt string) (string, error) {
+	// Create a request-scoped authentication client.
+	client, err := a.fbApp.Auth(ctx)
+	if err != nil {
+		// TODO: Find a way to differentiate between this error (HTTP 500) and the next one (HTTP 403)
+		return "", fmt.Errorf("error verifying authentication token: %w", err)
+	}
+
+	// Verify the token, returning an HTTP 403 if it cannot be.
+	verifiedToken, err := client.VerifyIDToken(ctx, jwt)
+	if err != nil {
+		return "", fmt.Errorf("failed to verify authentication token")
+	}
+
+	return verifiedToken.UID, nil
 }
--- backend/internal/user/middleware.go
+++ backend/internal/user/middleware.go
@@ -1,6 +1,9 @@
 package user
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // OrMiddleware is a middleware which makes a runtime decision between two provided middleware.  If cond evaluates to true, middleware a is used, else middleware b is used.
 func OrMiddleware(cond bool, a, b func(next http.Handler) http.Handler) func(next http.Handler) http.Handler {
@@ -8,4 +11,34 @@ func OrMiddleware(cond bool, a, b func(next http.Handler) http.Handler) func(nex
 		return a
 	}
 	return b
+}
+
+// AuthTokenVerifier is a middleware which verifies an Authorization header JWT using the Firebase Admin SDK.
+func AuthTokenVerifier() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		authenticator, err := NewAuthenticator()
+		if err != nil {
+			panic(fmt.Sprintf("error initializing auth middleware: %v", err))
+		}
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			userID, err := authenticator.VerifyJWT(r.Context(), r.Header.Get("Authorization"))
+			if err != nil {
+				http.Error(w, "failed to verify authentication token", http.StatusForbidden)
+				return
+			}
+
+			ctx := NewContextWithID(r.Context(), NewID(userID))
+			next.ServeHTTP(w, r.Clone(ctx))
+		})
+	}
+}
+
+// AuthStubber is a middleware which ignores the provided Authorization header and instead injects a static user ID into the request.
+func AuthStubber() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := NewContextWithID(r.Context(), NewID("testUserID"))
+			next.ServeHTTP(w, r.Clone(ctx))
+		})
+	}
 }
