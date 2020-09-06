91c0e3cdf659cab2b7b2cd09c1c20f3dd25815bc
Mario Leone <mleone10@gmail.com>
2020-09-05 22:38:29 -0400

    Add DynamoDB table configuration

--- backend/internal/dynamo/client.go
+++ backend/internal/dynamo/client.go
@@ -2,6 +2,12 @@ package dynamo
 
 import "github.com/mleone10/endpoint/internal/user"
 
+/*
+	Table schema (PK: userID:string, SK: sortKey:string)
+	API-Key schema (SK: KEY#key:string, Attrs: apiKey:string, name:string, readOnly:string)
+	GSI (PK: userID:string, SK: apiKey:string)
+*/
+
 // Client represents a DynamoDB accessor.
 type Client struct {
 }
@@ -14,12 +20,6 @@ func NewClient() (*Client, error) {
 
 // GetAPIKeys fetches a list of API keys for a given user, or an error if they cannot be retrieved.
 func (c *Client) GetAPIKeys(uid *user.ID) ([]user.APIKey, error) {
-	// TODO: Create DynamoDB table using Serverless framework
-	/*
-		Table schema (PK: userID:string, SK: sortKey:string)
-		API-Key schema (SK: KEY#key:string, Attrs: apiKey:string, name:string, readOnly:string)
-		GSI (PK: userID:string, SK: apiKey:string)
-	*/
 	// TODO: Call DynamoDB table to retrieve keys
 	return []user.APIKey{user.APIKey(uid.Value())}, nil
 }
